67d5184c8a5f7c39a0d8b885f7d40c9819013e61
Twitch Juana <tjuana@oa-l6.21-school.ru>
2020-03-06 17:57:26 +0300

    makefile:

--- inc/ft_printf.h
+++ inc/ft_printf.h
@@ -6,9 +6,19 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include "../libft/includes/libft.h"
+#include "libft.h"
+#include <math.h>
 
 int     ft_printf(const char *format, ...);
 int		parse_percent(char **format, va_list *arg);
+
+typedef struct			s_pf
+{
+	va_list				arg;
+	char				*str;
+	char				*buff;
+	size_t				buff_len;
+	// struct s_pf_spec	spec;
+}						t_pf;
 
 #endif
